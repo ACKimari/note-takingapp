97ceeef64814b7f53af59e40dc2990d8499a280a
A.C.Kimari <annchristine704@gmail.com>
2021-07-26 11:31:19 -0700

    worked on server.js file on the get and post functions

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,15 +1,15 @@
-let noteTitle = document.querySelector('.note-title');
-let noteText = document.querySelector('.note-textarea');
-let saveNoteBtn = document.querySelector('.save-note');
-let newNoteBtn = document.querySelector('.new-note');
-let noteList = document.querySelectorAll('.list-container .list-group');
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
 
 if (window.location.pathname === '/notes') {
-  noteTitle = document.querySelector('.note-title').value;
-  noteText = document.querySelector('.note-textarea').value;
-  saveNoteBtn = document.querySelector('.save-note').value;
-  newNoteBtn = document.querySelector('.new-note').value;
-  noteList = document.querySelectorAll('.list-container .list-group').value;
+  noteTitle = document.querySelector('.note-title');
+  noteText = document.querySelector('.note-textarea');
+  saveNoteBtn = document.querySelector('.save-note');
+  newNoteBtn = document.querySelector('.new-note');
+  noteList = document.querySelectorAll('.list-container .list-group');
 };
 
 // Show an element
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const notesData = require('./db/db.json');
+const fs = require('fs');
 
 const PORT = 3001;
 
@@ -10,18 +11,23 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
 
 app.get('/api/notes', (req, res) => {
     
     res.json(`${req.method} request received to get notes`);
-  
+    res.sendFile(path.join(__dirname, './db/db.json'));
     // Log our request to the terminal
     console.info(`${req.method} request received to get notes`);
   });
@@ -31,14 +37,17 @@ app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add notes`);
 
   // Destructuring assignment for the items in req.body
-  const { noteTitle, noteText } = req.body;
+  let userNote = {};
+  userNote.body = req.body.newNote;
 
   // If all the required properties are present
-  if (noteTitle && noteText) {
-    // Variable for the object we will save
-    const newNote = {
-      review_id: notesData(),
-    };
+  if (userNote.body.title && userNote.body.text) {
+    fs.readFileSync((path.join(__dirname, './db/db.json')), function (error, content) {
+        var data = JSON.parse(content);
+    });
+
+    // Lets say we have an array of numbers all 0 or greater. Each number only appears in the array once. How do we find the smallest number (that is still greater than 0) that is NOT in the array.
+
 
     const response = {
       status: 'success',
@@ -50,6 +59,8 @@ app.post('/api/notes', (req, res) => {
   } else {
     res.json('Error in posting notes');
   }
+
+  res.redirect('/');
 });
 
 app.delete('/api/notes/:id', (req, res) => {
