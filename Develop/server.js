1cdaedc56936ddd0b10aefc69c4b40dc98af0c65
A.C.Kimari <annchristine704@gmail.com>
2021-07-28 08:50:24 -0700

    Worked on delete button

--- Develop/server.js
+++ Develop/server.js
@@ -21,73 +21,95 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-  });
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 app.get('/api/notes', (req, res) => {
-    
-    res.json(`${req.method} request received to get notes`);
-    res.sendFile(path.join(__dirname, './db/db.json'));
-    // Log our request to the terminal
-    console.info(`${req.method} request received to get notes`);
-  });
+
+  // res.json(`${req.method} request received to get notes`);
+  res.sendFile(path.join(__dirname, './db/db.json'));
+  // Log our request to the terminal
+});
+
+function returnId(array) {
+  const sortedData = array
+    .sort(function (a, b) { return a.id - b.id });
+
+  for (let i = 1; i < sortedData.length; i++) {
+    if (sortedData[i - 1].id !== sortedData[i].id - 1) {
+      //if there's a gap
+      return sortedData[i - 1].id + 1;
+    }
+  };
+
+  return sortedData[sortedData.length - 1].id + 1
+}
 
 app.post('/api/notes', (req, res) => {
-  
+
   console.info(`${req.method} request received to add notes`);
 
   // Destructuring assignment for the items in req.body
-  let userNote = {};
-  userNote.body = req.body.newNote;
-
+  let userNote = {title : req.body.title, text: req.body.text};
+  console.log(req.body);
   // If all the required properties are present
-  if (userNote.body.title && userNote.body.text) {
-    fs.readFileSync((path.join(__dirname, './db/db.json')), function (error, content) {
-        var data = JSON.parse(content);
+  if (userNote.title && userNote.text) {
+
+    var d = fs.readFileSync('./db/db.json', function (error) {
+
     });
+    data = JSON.parse(d);
+    console.log(data);
+    if (data[0]) {
+      userNote.id = returnId(data);
+    }else {
+      userNote.id = 0
+    }
+    data.push(userNote);
 
-    // Lets say we have an array of numbers all 0 or greater. Each number only appears in the array once. How do we find the smallest number (that is still greater than 0) that is NOT in the array.
-
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    console.log(response);
-    res.json(response);
-  } else {
-    res.json('Error in posting notes');
+    let stringData = JSON.stringify(data);
+    fs.writeFile(path.join(__dirname, './db/db.json'), stringData, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Success!");
+    });
   }
 
   res.redirect('/');
 });
 
 app.delete('/api/notes/:id', (req, res) => {
+
+  console.info(`${req.method.id} request received to delete notes`);
   
-    console.info(`${req.method.id} request received to delete notes`);
-  
-    // Destructuring assignment for the items in req.body
-    const deleteNote = req.body;
-  
-    // If all the required properties are present
-    if (noteTitle && noteText) {
-      // Variable for the object we will save
-      const newNote = {
-        review_id: notesData(),
-      };
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.json(response);
-    } else {
-      res.json('Error in deleting notes');
-    }
+  // Destructuring assignment for the items in req.body
+  const deleteNote = req.params.id;
+  var d = fs.readFileSync('./db/db.json', function (error) {
+
   });
+  let data = JSON.parse(d);
+  let deleteNoteId;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == deleteNote){
+      deleteNoteId = i;
+      console.log(data[i]);
+    }
+  }
+  console.log(deleteNoteId);
+  data.splice(deleteNoteId, 1)
+
+  console.log(data);
+
+  let stringData = JSON.stringify(data);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), stringData, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Success!");
+    });
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
